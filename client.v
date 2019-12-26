7b880af8d92feca44a948bc8464b43b000fb3fa5
Minami <minami-git@minami-vr.sakura.ne.jp>
2019-12-26 23:38:14 +0900

    Update client.v

--- client.v
+++ client.v
@@ -7,14 +7,8 @@ import (
 
 fn main() {
   line := os.get_line()
-  sock := net.dial("localhost", 1234) or {
-    panic(err)
-  }
-  
-  sock.send(line.str, line.len) or {
-    panic(err)
-  }
-  
+  sock := net.dial("localhost", 1234)?
+  sock.send(line.str, line.len)?
   buf, n := sock.recv(line.len)
   ln := tos(buf, n)
   println(ln)
